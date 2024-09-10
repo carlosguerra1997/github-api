0309fcbc6daad1c9579fd99f9988391ff2738ade
Carlos Guerra <guerragcarlos97@gmail.com>
2024-09-10 18:36:38 +0200

    Implemented rest abstract class & class to manage exceptions

---
+++ src/Infrastructure/Services/Rest.ts
@@ -0,0 +1,48 @@
+import axios, { AxiosError, AxiosInstance, AxiosRequestConfig } from 'axios'
+
+import { RestException } from '@infrastructure/Services/RestException'
+import { RestRequest } from '@infrastructure/Services/RestRequest'
+import { RestResponse } from '@infrastructure/Services/RestResponse'
+
+export abstract class Rest {
+  protected constructor(
+    protected domain: string = '',
+    protected client: AxiosInstance = null
+  ) {
+    this.domain = domain
+
+    if (client === null) {
+      this.client = axios.create()
+    }
+  }
+
+  protected async call(request: RestRequest): Promise<RestResponse> {
+    try {
+      return await this.internalCall(request)
+    } catch (error) {
+      this.throwException(error)
+    }
+  }
+
+  private throwException(err: AxiosError): void {
+    const error = new RestException(err)
+    throw error
+  }
+
+  private async internalCall(request: RestRequest): Promise<any> {
+    const response = this.buildResponse()
+
+    const config = this.handleRequest(request)
+    const clientResponse = await this.client.request(config)
+    response.add(clientResponse)
+    return response
+  }
+
+  private buildResponse(): RestResponse {
+    return new RestResponse()
+  }
+
+  private handleRequest(request: RestRequest): AxiosRequestConfig {
+    return request.getConfig(this.domain)
+  }
+}
