0ad9a507a19d71cd469d3c0968398748f02773f8
Carlos Guerra <guerragcarlos97@gmail.com>
2024-09-19 17:41:11 +0200

    Deleted unecessary exception

--- src/Domain/Identity/Excception/NotFoundError.ts
+++
@@ -1,7 +0,0 @@
-import { HttpException, HttpStatus } from '@nestjs/common'
-
-export class NotFoundError extends HttpException {
-  constructor(message: string = 'Not Found') {
-    super(message, HttpStatus.NOT_FOUND)
-  }
-}
