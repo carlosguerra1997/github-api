6c91568eec5473f73b3e504147ed9cfa66a6726c
Carlos Guerra <guerragcarlos97@gmail.com>
2024-09-20 19:41:06 +0200

    Not found exception for user

--- src/Application/Github/Exception/GithubUserNotFoundException.ts
+++ src/Application/Github/Exception/GithubUserNotFoundException.ts
@@ -1,4 +1,4 @@
-import { NotFoundError } from '@domain/Identity/Excception/NotFoundError'
+import { NotFoundError } from '@domain/Identity/Exception/NotFoundError'
 
 export class GithubUserNotFoundException extends NotFoundError {
   constructor(message: string = 'User not found') {
