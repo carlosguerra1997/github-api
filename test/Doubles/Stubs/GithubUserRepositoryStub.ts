a514bc27f492e0077655bbcea5120a0f1e784c40
Carlos Guerra <guerragcarlos97@gmail.com>
2024-09-28 14:35:15 +0200

    Creating stub for github user repository

---
+++ test/Doubles/Stubs/GithubUserRepositoryStub.ts
@@ -0,0 +1,82 @@
+import { HttpStatus } from '@nestjs/common'
+
+import { GithubUser } from '@domain/Github/User/GithubUser'
+import { GithubUserRepository } from '@domain/Github/User/GithubUserRepository'
+
+import { GithubUserRestException } from '@infrastructure/Services/Github/Rest/User/GithubUserRestException'
+import { RestException } from '@infrastructure/Services/RestException'
+
+import { EntityRepositoryStub } from '@test/Doubles/Stubs/EntityRepositoryStub'
+import { mockAxiosError } from '@test/Doubles/Mocks/AxiosError'
+
+export class GithubUserRepositoryStub 
+  extends EntityRepositoryStub<GithubUser>
+  implements GithubUserRepository 
+{
+  private static readonly userKey = 'user'
+
+  constructor () {
+    super()
+
+    this.error = new Error('dummy error')
+    this.read = null
+    this.list = []
+  }
+
+  async searchUser(username: string): Promise<GithubUser> {
+    this.queryFilter = username
+
+    if (this.hasError) {
+      this.throwError()
+    }
+
+    return this.read
+  }
+
+  async searchUserFollowers(username: string): Promise<GithubUser[]> {
+    this.queryFilter = username
+
+    if (this.hasError) {
+      this.throwError()
+    }
+
+    return this.list
+  }
+
+  async searchUserFollowing(username: string): Promise<GithubUser[]> {
+    this.queryFilter = username
+
+    if (this.hasError) {
+      this.throwError()
+    }
+
+    return this.list
+  }
+
+  protected makeData(): void {
+    const githubUser = new GithubUser(
+      'https://github-avatar/carlosguerra.com',
+      'carlosguerra',
+      'Carlos Guerra',
+      'carlosguerra@gmail.com',
+      'Logroño',
+      'Hiberus',
+      10,
+      'https://github-profile/carlosguerra.com'
+    )
+
+    this.add(GithubUserRepositoryStub.userKey, githubUser)
+  }
+
+  protected throwError(): void {
+    if (!this.error) return
+
+    if (this.error instanceof RestException && this.error.code === HttpStatus.NOT_FOUND) {
+      const error = mockAxiosError('User not found', 404)
+
+      throw new GithubUserRestException(error)
+    }
+
+    throw new Error(this.error.message)
+  }
+}
