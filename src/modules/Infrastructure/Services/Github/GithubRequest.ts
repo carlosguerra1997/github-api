ecd3b2eb77cc8d3a5c39b42c2de4d0f60462810d
Carlos Guerra <guerragcarlos97@gmail.com>
2024-10-16 18:51:22 +0200

    Feat: github request

---
+++ src/modules/Infrastructure/Services/Github/GithubRequest.ts
@@ -0,0 +1,22 @@
+import { ListQuery } from '@common/Domain/Identity/List/ListQuery'
+
+import { RestRequest } from '@common/Infrastructure/Services/RestRequest'
+
+export class GithubRequest extends RestRequest {
+  constructor() {
+    super()
+    this.header('Accept', 'application/vnd.github.v3+json')
+  }
+
+  public static search(path: string): RestRequest {
+    const request = new GithubRequest()
+    request.read(path)
+    return request
+  }
+
+  public static list(path: string, query: ListQuery): RestRequest {
+    const request = new GithubRequest()
+    request.list(path, query)
+    return request
+  }
+}
